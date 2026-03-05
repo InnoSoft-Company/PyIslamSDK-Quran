302e87abcc94bea16269fc73ac8587e3238c5b43
MidoGhanam's Redmi 9T <mghanam883@gmail.com>
2026-03-05 08:13:46 +0200

    05-03-2026 | 08:13:46 | Improving CLI Commands & Uploading Release v0.1.2

--- pyislamsdk_quran/cli.py
+++ pyislamsdk_quran/cli.py
@@ -1,6 +1,12 @@
+import sys, subprocess, argparse
 from . import __version__
-import sys
 
 def main():
-  arg = sys.argv
-  if "--version" in arg: print(f"InnoCaptcha Version: {__version__}") 
+  parser = argparse.ArgumentParser(prog="pyislamsdk_quran", description="IslamSDK Quran module with Quranic texts, utilities, and local database.")
+  parser.add_argument("--version", action="version", version=f"PyIslamSDK-Quran (pyislamsdk_quran) Version: {__version__}", help="Show the current version")
+  parser.add_argument("--upgrade", action="store_true", help="Upgrade PyIslamSDK-Quran to the latest version")
+  args = parser.parse_args()
+  if args.upgrade:
+    print("Upgrading PyIslamSDK-Quran...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "PyIslamSDK-Quran"], shell=True)
+    print("Upgrade completed!")
